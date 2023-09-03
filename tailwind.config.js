ba03b8a88e42106b88e05c3190787d0e965a4ff5
Kiet Ly <kietly2k@gmail.com>
2023-09-03 19:38:01 +0700

    finish lession 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./build/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      // Define custom media queries
+      // TODO: research min-aspect-ratio
+      screens: { 
+        'widescreen': {'raw': '(min-aspect-ratio: 3/2)'},
+        'tallscreen': {'raw': '(min-aspect-ratio: 1/2)'},
+      }
+    },
   },
   plugins: [],
 }
